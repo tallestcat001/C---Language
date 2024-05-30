9a842799de1264fc25b1715ff19a04a672f0fab5
tallestcat001 <shdmlgus123_@naver.com>
2024-05-30 19:45:21 +0900

    (2024.5.30) 연관 컨테이너

---
+++ Class14th (Container Adapter)/Source.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+
+#pragma region 컨테이너 어댑터
+
+	// 기존의 컨테이너 인터페이스를 제한하여 만든 기능이 제한되거나 변형되어 있는 컨테이너
+
+#pragma region Stack
+
+	// stack<int> stack;
+
+	// stack.push(10);
+	// stack.push(20);
+	// stack.push(30);
+	// stack.push(40);
+	// stack.push(50);
+
+	// stack.pop();
+
+	// cout << "stack의 top 값은 :" << stack.top() << endl;
+	// cout << "stack의 size 값은 :" << stack.size() << endl;
+	// cout << "stack의 empty 값은 :" << stack.empty() << endl;
+
+	// int a = stack.size();
+
+	//	for (int i = 0; i < a; i++)
+	//	{
+	//		cout << "stack의 [ " << i << " ] 의 값은 : " << stack.top() << endl;
+	//		stack.pop();
+	//	}
+
+#pragma endregion
+
+#pragma region Queue
+
+	// queue<int> queue;
+
+	// queue.push(10);
+	// queue.push(20);
+	// queue.push(30);
+	// queue.push(40);
+	// queue.push(50);
+
+	// queue.pop();
+
+	// cout << "queue의 front 값은 :" << queue.front() << endl;
+	// cout << "queue의 size 값은 :" << queue.size() << endl;
+	// cout << "queue의 empty 값은 :" << queue.empty() << endl;
+
+	// int a = queue.size();
+
+	//	for (int i = 0; i < a; i++)
+	//	{
+	//		cout << queue.front() << " ";
+	//		queue.pop();
+	//	}
+
+#pragma endregion
+
+#pragma region Priority Queue
+
+	// priority_queue<int> priorityqueue;
+
+	// priorityqueue.push(10);
+	// priorityqueue.push(99);
+	// priorityqueue.push(2);
+	// priorityqueue.push(5);
+	// priorityqueue.push(11);
+
+	//	while (priorityqueue.empty() == false)
+	//	{
+	//		cout << priorityqueue.top() << endl;
+	//		priorityqueue.pop();
+	//	}
+
+#pragma endregion
+
+#pragma endregion
+
+	return 0;
+}
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,202 +1,25 @@
 ﻿#include <iostream>
-#include <time.h>
-#include <conio.h>
-#include <windows.h>
-#include <vector>
-#include <list>
-#include <deque>
-
-#define UP    72
-#define LEFT  75
-#define RIGHT 77
-#define DOWN  80
+#include <set>
 
 using namespace std;
-
-void Check(vector<const char *> & node, const char * arrow, int & health)
-{
-	if (node.back() == arrow) 
-	{
-		node.pop_back();
-	}
-	else
-	{
-		health--;
-	}
-}
-
-void View(int health)
-{
-	for (int i = 0; i < health; i++)
-	{
-		cout << "♥";
-	}
-
-	cout << endl;
-	cout << endl;
-
-}
 
 int main()
 {
 
-#pragma region 선형 컨테이너
+#pragma region 연관 컨테이너
 
-	// 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 일반적인 컨테이너.
+	// 컨테이너 내의 자료들이 규칙에 따라 정렬되어 있는 컨테이너.
 
-#pragma region Vector
+	set<int> set;
 
-	// vector<int> vector;
-
-	// vector.reserve(6);
-
-	// cout << "vector의 capacity값은 :" << vector.capacity() << endl;
-
-	// vector.push_back(10);
-	// vector.push_back(20);
-	// vector.push_back(30);
-	// vector.push_back(40);
-
-	// vector.pop_back();
+	set.insert(50);
+	set.insert(10);
+	set.insert(80);
+	set.insert(30);
+	set.insert(65);
+	set.insert(45);
 	
-	// for (int i = 0; i < vector.size(); i++)
-	// {
-	// 	  cout << "vector[" << i << "]의 값은 : " << vector[i] << endl;
-	// }
-
-#pragma endregion
-
-#pragma region Rhythm Game
-
-	// srand(time(NULL));
-
-	// vector<const char*> node;
-	// char key = 0;
-	// int count = 4;
-	// int health = 5;
-
-	// node.reserve(6);
-
-	//	for (int i = 0; i < count; i++)
-	//	{
-	//		int random = rand() % 4;
-	//	
-	//		switch (random)
-	//		{
-	//		case 0 : node.push_back("↑");
-	//			break;
-	//		case 1 : node.push_back("←");
-	//			break;
-	//		case 2 : node.push_back("→");
-	//			break;
-	//		case 3 : node.push_back("↓");
-	//			break;
-	//		}
-	//	}
-
-	//	while (health > 0)
-	//	{
-	//		View(health);
-	//	
-	//		for (const char* element : node)
-	//		{
-	//			cout << element << "  ";
-	//		}
-	//	
-	//		key = _getch();
-	//	
-	//		if (key == -32)
-	//		{
-	//			key = _getch();
-	//		}
-	//	
-	//		switch (key)
-	//		{
-	//		case UP: Check(node, "↑", health);
-	//			break;
-	//		case LEFT : Check(node, "←", health);
-	//			break;
-	//		case RIGHT : Check(node, "→", health);
-	//			break;
-	//		case DOWN : Check(node, "↓", health);
-	//			break;
-	//		}
-	//	
-	//		if (node.size() <= 0)
-	//		{
-	//			break;
-	//		}
-	//	
-	//		system("cls");
-	//	
-	//	}
-
-	// system("cls");
-
-	//	if (health <= 0)
-	//	{
-	//		cout << "Defeat" << endl;
-	//	}
-	//	else
-	//	{
-	//		cout << "Victory" << endl;
-	//	}
-
-#pragma endregion
-
-#pragma region List
-
-	// list<int> list;
-
-	// list.push_back(10);
-	// list.push_back(20);
- 
-	// list.push_front(5);
-	// list.push_front(1);
- 
-	// list.pop_back();
-	// list.pop_front();
- 
-	// cout << "list의 front : " << list.front() << endl;
-	// cout << "list의 back  : " << list.back() << endl;
-
-#pragma endregion
-
-#pragma region String
-
-	// string name = "Alistar";
-
-	// name.append(" Champion");
-
-	//	if (name.compare("Alistar"))
-	//	{
-	//		cout << "문자열이 같습니다." << endl;
-	//	}
-
-	// name.clear();
-
-	// cout << "name 변수의 값은 : " << name << endl;
-
-#pragma endregion
-
-#pragma region Deque
-
-	// deque<int> deque;
-
-	// deque.push_front(10);
-	// deque.push_back(20);
-	// deque.push_back(30);
-	// deque.push_back(40);
-
-	// deque.pop_back();
-	// deque.pop_front();
-
-	//	for (int i = 0; i < deque.size(); i++)
-	//	{
-	//		cout << "deque[" << i << "] 값은 : " << deque[i] << endl;
-	//	}
-
-#pragma endregion
+	// set의 경우 Key값이 중복일 경우 값이 그대로 들어옴.
 
 #pragma endregion
 
