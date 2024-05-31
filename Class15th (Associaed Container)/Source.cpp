5c00963e899902369b5faf033429025843d038c9
tallestcat001 <shdmlgus123_@naver.com>
2024-05-31 19:47:25 +0900

    (2024.5.31) Final

---
+++ Class15th (Associaed Container)/Source.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+
+#pragma region 연관 컨테이너
+
+	// 컨테이너 내의 자료들이 규칙에 따라 정렬되어 있는 컨테이너.
+
+#pragma region 반복자
+
+	// 컨테이너 내의 원소를 순회하기 위해 사용되는 도구.
+
+	// set<int> Set;
+
+	// set<int>::iterator iterator;
+
+	// Set.insert(50);
+	// Set.insert(10);
+	// Set.insert(80);
+	// Set.insert(30);
+	// Set.insert(65);
+	// Set.insert(45);
+
+	//	for (iterator = Set.begin(); iterator != Set.end(); iterator++)
+	//	{
+	//		cout << *iterator << endl;
+	//	}
+
+	// set의 경우 Key값이 중복일 경우 값이 그대로 들어옴.
+
+#pragma endregion
+
+#pragma region Map
+
+	// map<string, int> Map;
+
+	// map<string,int>::iterator iterator;
+
+	// Map.insert({ "Sword", 10000 });
+	// Map.insert({ "Armor", 7500 });
+
+	//	if (Map.find("Sword") != Map.end())
+	//	{
+	//		cout << "Find" << endl;
+	//	}
+	//	else
+	//	{
+	//		cout << "Not Find" << endl;
+	//	}
+
+	// Map.erase("Sword");
+
+	//	for (iterator = Map.begin(); iterator != Map.end(); iterator++)
+	//	{
+	//		cout << "Key : " << iterator->first << endl;
+	//		cout << "Value :" << iterator->second << endl;
+	//	}
+
+#pragma endregion
+
+#pragma endregion
+
+	return 0;
+}
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,25 +1,105 @@
 ﻿#include <iostream>
-#include <set>
 
 using namespace std;
+
+class Item
+{
+private:
+	int price;
+public:
+	Item(int price)
+	{
+		cout << "Constructor" << endl;
+		this->price = price;
+	}
+	Item(Item& item)
+	{
+		cout << "Copy Constructor" << endl;
+		price = item.price;
+	}
+};
+
+class Card
+{
+public:
+	virtual void show() = 0;
+	virtual void skill() = 0;
+	virtual void effect() = 0;
+
+};
+
+class Legend : Card
+{
+public:
+	void show()  override
+	{
+		cout << "Legend Card" << endl;
+	}
+	void skill() override
+	{
+		cout << "Legend skill" << endl;
+	}
+	void effect() final
+	{
+		cout << "Legend effect" << endl;
+	}
+};
+
+class Unqiue : Legend
+{
+public:
+	Unqiue()
+	{
+		effect();
+	}
+};
 
 int main()
 {
 
-#pragma region 연관 컨테이너
+#pragma region R value and L value
 
-	// 컨테이너 내의 자료들이 규칙에 따라 정렬되어 있는 컨테이너.
+	// L value type
+	/*
+	int data = 10;
 
-	set<int> set;
+	cout << data << endl;
 
-	set.insert(50);
-	set.insert(10);
-	set.insert(80);
-	set.insert(30);
-	set.insert(65);
-	set.insert(45);
-	
-	// set의 경우 Key값이 중복일 경우 값이 그대로 들어옴.
+	int& left1 = data;
+
+	left1= 20;
+
+	cout << data << endl;
+	*/
+
+	// R value type
+	/*
+	int count = 0;
+
+	int&& right1 = 10;
+
+	cout << "right1의 값은 : " << right1 << endl;
+
+	right1 = 30;
+
+	cout << "right1의 값은 : " << right1 << endl;
+	*/
+
+#pragma endregion
+
+#pragma region 복사 생략 (Copy Elision)
+
+	// 함수의 반환 값을 모두 사용하거나 초기화하는 경우에 생기는 불필요한 임시 객체를 최적화 하거나 제거하는데 사용되는 컴파일러 기술.
+
+	// Item item1(10000);
+
+	// Item item2(item1);
+
+#pragma endregion
+
+#pragma region Final
+
+
 
 #pragma endregion
 
