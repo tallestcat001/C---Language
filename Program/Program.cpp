0906b49660010a1c4ec2482780a2660b1d11c8bd
tallestcat001 <shdmlgus123_@naver.com>
2024-06-04 19:45:27 +0900

    (2024.6.4) 인벤토리시스템2

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -16,6 +16,19 @@ void Position(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
 
+class Item
+{
+private:
+	int price;
+	const char* name;
+public:
+	void SetData(int price, const char* name)
+	{
+		this->price = price;
+		this->name = name;
+	}
+};
+
 class Input
 {
 private:
@@ -38,7 +51,7 @@ public:
 		cout << shape;
 	}
 
-	void GetKey()
+	void GetKey(int & index)
 	{
 		key = _getch();
 
@@ -49,14 +62,25 @@ public:
 
 		switch (key)
 		{
-			case UP: y -= 2;
+		case UP: if (y > 1) { y -= 2; index -= 8; }
 				break;
-			case LEFT: x -= 2;
+		case LEFT: if (x > 0) { x -= 2; index -= 2; }
 				break;
-			case RIGHT: x += 2;
+		case RIGHT: if (x < 6) { x += 2; index += 2; }
 				break;
-			case DOWN: y += 2;
+		case DOWN: if (y < 5) { y += 2; index += 8; }
 				break;
+		}
+	}
+
+	void Select(Item *item[],int & index)
+	{
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			if (item[index / 2] == nullptr)
+			{
+				item[index / 2] = new Item();
+			}
 		}
 	}
 };
@@ -66,17 +90,26 @@ class Inventory
 private:
 	int size;
 	int width;
+	int index;
 	Input input;
+	Item *item[12];
 public:
 	Inventory(int size, int width)
 	{
 		this->size = size;
 		this->width = width;
+		index = 0;
+
+		for (int i = 0; i < 12; i++)
+		{
+			item[i] = nullptr;
+		}
 	}
 
 	void UpDate()
 	{
-		input.GetKey();
+		input.GetKey(index);
+		input.Select(item,index);
 	}
 
 	void Renderer()
@@ -89,13 +122,29 @@ public:
 			{
 				cout << endl << endl;
 			}
-			cout << "□";
+			if (item[i] == nullptr)
+			{
+				cout << "□";
+			}
+			else
+			{
+				cout << "■";
+			}
 		}
 		input.Renderer();
 	}
+
+	~Inventory()
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			if(item[i] != nullptr)
+			{ 
+				delete item[i];
+			}
+		}
+	}
 };
-
-
 
 int main()
 {
@@ -103,11 +152,11 @@ int main()
 #pragma region 인벤토리
 
 	Inventory inventory(12, 4);
-
+	
 	while (true)
 	{
 		inventory.Renderer();
-
+	
 		inventory.UpDate();
 
 		system("cls");
